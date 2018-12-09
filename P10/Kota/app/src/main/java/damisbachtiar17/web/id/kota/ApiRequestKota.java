522724a8ff5aa8bc3989c45fca542525d3fac23b
Damis Bachtiar <34196185+araskeren@users.noreply.github.com>
2018-12-09 21:23:54 +0700

    Revert "P8-P10"

--- P10/Kota/app/src/main/java/damisbachtiar17/web/id/kota/ApiRequestKota.java
+++
@@ -1,9 +0,0 @@
-package damisbachtiar17.web.id.kota;
-
-import retrofit2.Call;
-import retrofit2.http.GET;
-
-public interface ApiRequestKota {
-    @GET("read.php")
-    Call<ResponsModel> getKota();
-}
